11859741f36086d8bf62267b02cafec019ed94dd
bunny <67633271+bunnykek@users.noreply.github.com>
2022-03-21 10:41:45 +0530

    Minor fixes and restrictions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Made by bunny
+
 const fs = require('fs');
 const { Client, LegacySessionAuth } = require('whatsapp-web.js');
 const { connect_to_db, UpdateMessage_ID, mongodb_initialize } = require('./Scripts/MongoDB');
@@ -25,7 +27,9 @@ async function main() {
 
     let owner = process.env.owner + '@c.us';
 
-    let Collection = await connect_to_db()
+    let Collection = await connect_to_db();
+
+    let bot_status = 0;
 
     global_id.id = await mongodb_initialize(Collection)
     console.log('Global ID : ',global_id.id)
@@ -40,19 +44,29 @@ async function main() {
 
     client.initialize();
 
-
     client.on('ready', () => {
-        console.log('Whatsapp client is ready!\nWaiting for new emails to arrive :D ...');
-        setInterval(submain, 60000)
+        console.log('Send !start command to the bot using the owners number.');
     });
 
     client.on('authenticated', (session) => {
-        console.log("Whatsapp authenticated")
+        console.log("Whatsapp authenticated.")
     });
 
     client.on('message', async msg => {
-        if (msg.body === '!ping') {
-            msg.reply('pong');
+        if(msg.from == owner && msg.body == '!start') {
+            if(bot_status == 0) {
+                bot_status = 1;
+                msg.reply(`\`\`\`Bot has been started and will send the last 10 emails if its the first time otherwise it will wait for the arrival of new emails!\`\`\``);
+                console.log('Waiting for new emails to arrive :D ...');
+                setInterval(submain, 60000)
+            }
+            else {
+                msg.reply(`\`\`\`beep! boop!\`\`\``);
+            }
+        }
+
+        if (msg.body === '!ping' && msg.from == owner) {
+            msg.reply('pong!');
         }
     });
 
